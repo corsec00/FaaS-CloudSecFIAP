223d1337e9183512ed8f7210eab9fbabe32fd271
Leo Santos <corsec00@gmail.com>
2024-02-13 13:59:03 -0300

    Add files via upload

---
+++ Recorrente.cs
@@ -0,0 +1,31 @@
+ using System;
+ using Microsoft.Azure.Functions.Worker;
+ using Microsoft.Extensions.Logging;
+ namespace func
+ {
+     public class Recorrente
+     {
+         private readonly ILogger _logger;
+         public Recorrente(ILoggerFactory loggerFactory)
+         {
+             _logger = loggerFactory.CreateLogger<Recorrente>();
+         }
+         [Function("Recorrente")]
+         public void Run([TimerTrigger("0 */2 * * * *")] MyInfo myTimer)
+         {
+             _logger.LogInformation($"Function C# de disparo por tempo foi executado à: {DateTime.Now}");
+             _logger.LogInformation($"Próxima execução será à: {myTimer.ScheduleStatus.Next}");
+         }
+     }
+     public class MyInfo
+     {
+         public MyScheduleStatus ScheduleStatus { get; set; }
+         public bool IsPastDue { get; set; }
+     }
+     public class MyScheduleStatus
+     {
+         public DateTime Last { get; set; }
+         public DateTime Next { get; set; }
+         public DateTime LastUpdated { get; set; }
+     }
+ }
